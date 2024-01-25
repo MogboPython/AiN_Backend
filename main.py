2f4c4465e9583218dfa6b7b42f177f06e6380afc
MogboPython <mogbo18@yahoo.com>
2024-01-25 22:46:55 +0100

    creating function that checks emails

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
 from utils.helper_functions import split_name, split_university
 from utils.mail import send_email_async
-from utils.sheet import gc
+from utils.sheet import gc, check_email
 
 
 app = FastAPI()
@@ -31,7 +31,7 @@ async def validation_exception_handler(request, exc):
 
 def submit_to_sheet(email: str):
     sheet = gc.open('Website Email Signups').sheet1
-    emails = [item for item in sheet.col_values(1) if item]
+    emails = {item for item in sheet.col_values(1) if item}
 
     if email in emails:
         raise HTTPException(status_code=400, detail="already registered")
@@ -47,13 +47,10 @@ def submit_recruit(recruit: Recruit):
         name_of_school = university
         school_location = state
 
-    sheet = gc.open('AiN 2024 Recruitment Sheet').sheet1
-    all_values = sheet.get_all_values()
-    emails = [row[3] for row in all_values]  
+    sheet = gc.open('AiN 2024 Recruitment Sheet').sheet1 
+    check_registered_email = check_email(sheet, 3, recruit.email)
 
-    if recruit.email in emails:
-        raise HTTPException(status_code=400, detail="already registered")
-    else:
+    if check_registered_email == None:
         sheet.append_row([
             recruit.first_name,
             recruit.last_name,
@@ -79,12 +76,9 @@ def submit_recruit(recruit: Recruit):
 # Callback function for the ncjos endpiont
 def submit_nc_jos_data(ncjos: NCJOS):
     sheet = gc.open('NC Jos Conference Signups').sheet1
-    all_values = sheet.get_all_values()
-    emails = [row[3] for row in all_values]  
+    check_registered_email = check_email(sheet, 3, ncjos.email)
 
-    if ncjos.email in emails:
-        raise HTTPException(status_code=400, detail="This user already exists")
-    else:
+    if check_registered_email == None:
         created_at = datetime.now().strftime("%d/%m/%Y %H:%M")
         sheet.append_row([
             ncjos.name,
@@ -106,25 +100,6 @@ def submit_nc_jos_data(ncjos: NCJOS):
         first_name, last_name = split_name(ncjos.name)
 
         return first_name, last_name, ncjos.email, ncjos.lc
-    
-def submit_guest(guest: Guest):
-    sheet = gc.open('Eclipse Dinner Guests').sheet1
-    all_values = sheet.get_all_values()
-    emails = [row[1] for row in all_values]  
-
-    if guest.email in emails:
-        raise HTTPException(status_code=400, detail="already registered")
-    else:
-        sheet.append_row([
-            guest.name,
-            guest.email,
-            guest.ticket_type,
-            guest.other_names,
-            guest.suggestions
-        ])
-
-        return guest.name
-
 
 @app.get("/api/healthcheck")
 def read_root():
@@ -167,11 +142,6 @@ async def register(recruit: Recruit):
         'recruitment_email_template.html'
     )
     return {"detail":"registration success"}  
-
-@app.post("/api/dinner_registration")
-async def dinner_registration(guest: Guest):
-    _ = submit_guest(guest)
-    return {"detail":"registration success"}
 
 if __name__ == "__main__":
     app.run()
--- utils/sheet.py
+++ utils/sheet.py
@@ -1,6 +1,7 @@
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 from dotenv import load_dotenv
+from fastapi.exceptions import HTTPException
 import os
 load_dotenv()
 
@@ -14,3 +15,13 @@ GOOGLE_OAUTH_SCOPES = [
 GOOGLE_CREDENTIALS = os.getenv('CREDENTIALS')
 
 gc = gspread.authorize(ServiceAccountCredentials.from_json_keyfile_name(GOOGLE_CREDENTIALS, GOOGLE_OAUTH_SCOPES))
+
+"""Checks if an email address is already registered"""
+def check_email(sheet: gspread.worksheet.Worksheet, column: int, email: str):
+  all_values = sheet.get_all_values()
+  emails = {row[column] for row in all_values}  
+
+  if email in emails:
+      raise HTTPException(status_code=400, detail="already registered")
+  else:
+     return None
